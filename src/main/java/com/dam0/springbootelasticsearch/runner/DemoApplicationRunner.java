774416d2358507f0706b2fb2991a93991fe9943f
2dam0 <yidamyeong@gmail.com>
2022-03-24 19:13:36 +0900

    modify the way to handle exception in DemoApplicationRunner

--- src/main/java/com/dam0/springbootelasticsearch/runner/DemoApplicationRunner.java
+++ src/main/java/com/dam0/springbootelasticsearch/runner/DemoApplicationRunner.java
@@ -6,17 +6,21 @@ import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.*;
 
 @Order(2)
 @Component
 public class DemoApplicationRunner implements ApplicationRunner {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplicationRunner.class);
     private List<String> fieldList;
     private final SearchAPIs searchAPIs;
 
@@ -25,11 +29,18 @@ public class DemoApplicationRunner implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         // keyword 붙이고, 안 붙이고의 차이에 대해.
         // size, page 미표기하여 전부 검색할 수는 없을까?
         MatchQueryBuilder query = QueryBuilders.matchQuery("field1.keyword", "sample-value");
-        SearchResponse response = searchAPIs.search("sample-index", query, "field2.keyword", 1, 100);
+        SearchResponse response;
+        try {
+            response = searchAPIs.search("sample-index", query, "field2.keyword", 1, 100);  // IOException
+        } catch (IOException e) {
+            LOGGER.error("## IOException Occurred");
+            LOGGER.error("# FAILED LOADING SAMPLE-FIELD-LIST", e);
+            return;
+        }
 
         SearchHits searchHits = response.getHits();
 
@@ -37,10 +48,17 @@ public class DemoApplicationRunner implements ApplicationRunner {
         List<String> fieldList = new ArrayList<>();
         for (SearchHit searchHit : searchHitList) {
             Map<String, Object> source = searchHit.getSourceAsMap();
-            fieldList.add((String) source.get("sample-field"));
+            fieldList.add((String) source.get("field2"));
         }
 
+        /* 또다른 방법
+        Arrays.stream(searchHits.getHits())
+                .map(a -> (String) a.getSourceAsMap().get("sample-field"))
+                .forEach(fieldList::add);
+         */
+
         this.fieldList = fieldList;
+        LOGGER.debug("fieldList = {}", fieldList);
     }
 
     public List<String> getList() {
